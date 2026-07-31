3e55297fe5c714f5fd3a7dc5c84a14db24a5bc4a
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2026-07-31 21:25:58 +0530

    Day-66 module download error resolved today but now error with AWS EKS creation

--- 2026/day-66/terraform-eks/variables.tf
+++ 2026/day-66/terraform-eks/variables.tf
@@ -13,7 +13,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for EKS"
   type        = string
-  default     = "1.31"
+  default     = "1.36"
 }
 
 variable "node_instance_type" {
