016d4889433055a68b1dc5e82b9295a565011040
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2026-08-19 18:54:44 +0530

    Day-71 task-1 AWS infra created with all three DB, App, Web ec2 instance

---
+++ 2026/day-71/ansible-lab-tf/outputs.tf
@@ -0,0 +1,12 @@
+output "web_public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+
+output "db_public_ip" {
+  value = aws_instance.db.public_ip
+}
+
+output "app_public_ip" {
+  value = aws_instance.app.public_ip
+}
